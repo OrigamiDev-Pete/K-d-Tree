a9aa2dbfea7e1697a46931e022c3693b77d4dc73
Peter de Vroom <pdevroom@outlook.com>
2023-09-24 14:33:48 +1000

    Add insert and tests

--- src/kdTree.zig
+++ src/kdTree.zig
@@ -12,7 +12,7 @@ pub const KPoint = struct {
     }
 
     pub fn kCompare(self: KPoint, other: KPoint, k: u32) f32 {
-        return self.value[k] < other.value[k];
+        return other.value[k] - self.value[k];
     }
 
     pub fn dimensions(self: KPoint) u32 {
@@ -26,59 +26,71 @@ pub const KBoundingRegion = struct {
 };
 
 pub const KDTree = struct {
-
     const KDNode = struct {
-        leftChild: ?*KDNode,
-        rightChild: ?*KDNode,
+        left_child: ?*KDNode = null,
+        right_child: ?*KDNode = null,
         point: KPoint,
-        // Note(Pete): direction determines the dimension in the hyperspace to use when comparing. 
+        // Note(Pete): direction determines the dimension in the hyperspace to use when comparing.
         // Other implementations may store a level and compute the direction based on the dimensionality of the tree.
-        direction: u32, 
-        allocator: ?std.mem.Allocator,
+        direction: u32 = 0,
+        allocator: ?std.mem.Allocator = null,
 
         fn search(self: KDNode, point: KPoint) bool {
-            if (self.point.equals(point)) return true
-            else if (self.point.kCompare(point, self.direction) < 0 and self.leftChild) {
-                return self.leftChild.search(point);
-            } 
-            else if (self.rightChild) {
-                return self.rightChild.search(point);
-            }
-            else return false;
+            if (self.point.equals(point)) return true else if (self.point.kCompare(point, self.direction) < 0 and self.left_child) {
+                return self.left_child.search(point);
+            } else if (self.right_child) {
+                return self.right_child.search(point);
+            } else return false;
         }
 
-        fn insert(self: KDNode, point: KPoint, direction: u32, k: u32, allocator: std.mem.Allocator) KDNode {
+        fn insert(self: *KDNode, point: KPoint, direction: u32, k: u32, allocator: std.mem.Allocator) !*KDNode {
             const newDirection = (direction + 1) % k;
-            if (self.point.equals(point)) return self
-            else if (self.point.kCompare(point, direction) < 0) {
-                if (self.leftChild) {
-                    self.leftChild = self.leftChild.insert(point, newDirection);
+            if (self.point.equals(point)) return self else if (self.point.kCompare(point, direction) < 0) {
+                if (self.left_child != null) {
+                    self.left_child = try self.left_child.?.insert(point, newDirection, k, allocator);
                     return self;
                 } else {
-                    return allocator.create(KDNode{ .point = point, .direction = direction, .allocator = allocator });
+                    var node = try allocator.create(KDNode);
+                    node.* = KDNode{ .point = point, .direction = direction };
+                    self.left_child = node;
+                    return node;
                 }
-            } else { 
-                if (self.rightChild) {
-                    self.rightChild = self.rightChild.insert(point, newDirection);
+            } else {
+                if (self.right_child != null) {
+                    self.right_child = try self.right_child.?.insert(point, newDirection, k, allocator);
                     return self;
                 } else {
-                    return allocator.create(KDNode{ .point = point, .direction = direction, .allocator = allocator });
+                    var node = try allocator.create(KDNode);
+                    node.* = KDNode{ .point = point, .direction = direction };
+                    self.right_child = node;
+                    return node;
                 }
             }
         }
-
     };
 
     root: ?*KDNode,
     k: u32, // Number of dimensions
     allocator: std.mem.Allocator,
-    
-    pub fn init(points: []KPoint, alloctor: std.mem.Allocator) KDTree {
-        return KDTree{
-            .root = null,
-            .k = points[0].dimensions(),
-            .allocator = alloctor
-        };
+
+    pub fn init(points: []KPoint, alloctor: std.mem.Allocator) !KDTree {
+        var tree = KDTree{ .root = null, .k = points[0].dimensions(), .allocator = alloctor };
+        for (points) |point| {
+            _ = try tree.insert(point);
+        }
+        return tree;
+    }
+
+    pub fn deinit(self: KDTree) void {
+        self._deinit(self.root);
+    }
+
+    fn _deinit(self: KDTree, node: ?*KDNode) void {
+        if (node) |n| {
+            self._deinit(n.left_child);
+            self._deinit(n.right_child);
+            self.allocator.destroy(n);
+        }
     }
 
     pub fn size(self: KDTree) u32 {
@@ -90,10 +102,25 @@ pub const KDTree = struct {
         return self.size() == 0;
     }
 
-    pub fn insert(self: KDTree, point: KPoint) KDNode {
-        if (self.root == null) 
-            return self.allocator.create(KDNode{ .point = point, .level = 0, .allocator = self.allocator });
-        return self.root.insert(point, 0, self.allocator);
+    pub fn insert(self: *KDTree, point: KPoint) !*KDNode {
+        self.root = try self._insert(self.root, point, 0);
+        return self.root.?;
+    }
+
+    fn _insert(self: KDTree, node: ?*KDNode, point: KPoint, direction: u32) !*KDNode {
+        const newDirection = (direction + 1) % self.k;
+        if (node == null) {
+            const newNode = try self.allocator.create(KDNode);
+            newNode.* = KDNode{ .point = point, .direction = direction };
+            return newNode;
+        } else if (node.?.point.equals(point)) { return node.?; }
+        else if (node.?.point.kCompare(point, direction) < 0) {
+            node.?.left_child = try self._insert(node.?.left_child, point, newDirection);
+            return node.?;
+        } else {
+            node.?.right_child = try self._insert(node.?.right_child, point, newDirection);
+            return node.?;
+        }
     }
 
     pub fn remove(self: KDTree, point: KPoint) void {
@@ -116,4 +143,5 @@ pub const KDTree = struct {
         _ = region;
         _ = self;
     }
+
 };
--- src/main.zig
+++ src/main.zig
@@ -3,25 +3,123 @@ const kdt = @import("kdTree.zig");
 const KDTree = kdt.KDTree;
 const KPoint = kdt.KPoint;
 
+const raylib = @import("raylib");
+
 var gpa = std.heap.GeneralPurposeAllocator(.{}){};
 const allocator = gpa.allocator();
 
+var screenWidth: i32 = 800;
+var screenHeight: i32 = 600;
+
 pub fn main() !void {
-    var point = KPoint{ .value = &.{ 1.0 } };
-    var points = [_]KPoint{ point };
-    const tree = KDTree.init(points[0..], allocator);
+    // raylib.SetConfigFlags(raylib.ConfigFlags{ .FLAG_WINDOW_RESIZABLE = true });
+    // raylib.InitWindow(screenWidth, screenHeight, "hello world!");
+    // raylib.SetTargetFPS(60);
+    // defer raylib.CloseWindow();
+
+    var points = [_]KPoint{ 
+        KPoint{ .value = &.{ 0.0, 5.0 } }, 
+        KPoint{ .value = &.{ 1.0, -1.0 } },
+        KPoint{ .value = &.{ -1.0, 6.0 } },
+        KPoint{ .value = &.{ -1.0, 1.0 } },
+        KPoint{ .value = &.{ 2.0, -5.0 } },
+        KPoint{ .value = &.{ -0.5, 0.0 } },
+    };
+    var tree = try KDTree.init(points[0..], allocator);
+    _ = try tree.insert(KPoint{ .value = &.{ -1.5, -2 } });
     _ = tree.isEmpty();
 
+    var t = KDTree{ .root = null, .k = 1, .allocator = allocator };
+    defer t.deinit();
+    const node_a = try t.insert(KPoint { .value = &.{ 0.0 } });
+    _ = node_a;
+
+    const node_b = try t.insert(KPoint { .value = &.{ 5.0 } });
+    _ = node_b;
+
+    const node_c = try t.insert(KPoint { .value = &.{ 3.0 } });
+    _ = node_c;
+    _ = t.isEmpty();
+
+    // while (!raylib.WindowShouldClose()) {
+    //     if (raylib.IsWindowResized()) {
+    //         screenWidth = raylib.GetScreenWidth();
+    //         screenHeight = raylib.GetScreenHeight();
+    //     }
+
+    //     const screenXMiddle = @divTrunc(screenWidth, 2);
+    //     const screenYMiddle = @divTrunc(screenHeight, 2);
+
+    //     raylib.BeginDrawing();
+    //     defer raylib.EndDrawing();
+
+    //     raylib.DrawLine(screenXMiddle, 0, screenXMiddle, screenHeight, raylib.GRAY);
+    //     raylib.DrawLine(0, screenYMiddle, screenWidth, screenYMiddle, raylib.GRAY);
+
+    //     for (points) |p| {
+    //         raylib.DrawCircle(@as(i32, @intFromFloat(p.value[0])) + screenXMiddle, screenYMiddle - @as(i32, @intFromFloat(p.value[1])), 3, raylib.RED);
+    //     }
+
+    //     raylib.ClearBackground(raylib.WHITE);
+    //     raylib.DrawFPS(10, 10);
+    // }
 }
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
+test "Insert 1D" {
+    const testing_allocator = std.testing.allocator;
+    var tree = KDTree{ .root = null, .k = 1, .allocator = testing_allocator };
+    defer tree.deinit();
+    const root = try tree.insert(KPoint { .value = &.{ 0.0 } });
+    try std.testing.expectEqualSlices(f32, &.{ 0.0 }, root.point.value);
+    try std.testing.expectEqual(@as(u32, 0), root.direction);
+
+    _ = try tree.insert(KPoint { .value = &.{ 5.0 } });
+    try std.testing.expectEqualSlices(f32, &.{ 5.0 }, root.right_child.?.point.value);
+    try std.testing.expectEqual(@as(u32, 0), root.right_child.?.direction);
+
+    _ = try tree.insert(KPoint { .value = &.{ 3.0 } });
+    try std.testing.expectEqualSlices(f32, &.{ 3.0 }, root.right_child.?.left_child.?.point.value);
+    try std.testing.expectEqual(@as(u32, 0), root.right_child.?.left_child.?.direction);
+
+    _ = try tree.insert(KPoint { .value = &.{ -1.0 } });
+    try std.testing.expectEqualSlices(f32, &.{ -1.0 }, root.left_child.?.point.value);
+    try std.testing.expectEqual(@as(u32, 0), root.left_child.?.direction);
 }
 
-test "example test" {
-    try std.testing.expectEqual(39, 39);
+test "Insert 2D" {
+    const testing_allocator = std.testing.allocator;
+    var tree = KDTree{ .root = null, .k = 2, .allocator = testing_allocator };
+    defer tree.deinit();
+    const root = try tree.insert(KPoint { .value = &.{ 0.0, 0.0 } });
+    try std.testing.expectEqualSlices(f32, &.{ 0.0, 0.0 }, root.point.value);
+    try std.testing.expectEqual(@as(u32, 0), root.direction);
+
+    _ = try tree.insert(KPoint { .value = &.{ 5.0, 2.0 } });
+    try std.testing.expectEqualSlices(f32, &.{ 5.0, 2.0 }, root.right_child.?.point.value);
+    try std.testing.expectEqual(@as(u32, 1), root.right_child.?.direction);
+
+    _ = try tree.insert(KPoint { .value = &.{ 3.0, 1.0 } });
+    try std.testing.expectEqualSlices(f32, &.{ 3.0, 1.0 }, root.right_child.?.left_child.?.point.value);
+    try std.testing.expectEqual(@as(u32, 0), root.right_child.?.left_child.?.direction);
+
+    _ = try tree.insert(KPoint { .value = &.{ -1.0, -3.0 } });
+    try std.testing.expectEqualSlices(f32, &.{ -1.0, -3.0 }, root.left_child.?.point.value);
+    try std.testing.expectEqual(@as(u32, 1), root.left_child.?.direction);
 }
 
+test "Init Tree" {
+    const testing_allocator = std.testing.allocator;
+    var points = [_]KPoint{ 
+        KPoint{ .value = &.{ 0.0, 5.0 } }, 
+        KPoint{ .value = &.{ 1.0, -1.0 } },
+        KPoint{ .value = &.{ -1.0, 6.0 } },
+        KPoint{ .value = &.{ -1.0, 1.0 } },
+        KPoint{ .value = &.{ 2.0, -5.0 } },
+        KPoint{ .value = &.{ -0.5, 0.0 } },
+    };
+    var tree = try KDTree.init(points[0..], testing_allocator);
+    defer tree.deinit();
+
+    try std.testing.expectEqualSlices(f32, &.{ 2.0, -5.0 }, tree.root.?.right_child.?.left_child.?.point.value);
+    try std.testing.expectEqualSlices(f32, &.{ -0.5, 0.0 }, tree.root.?.left_child.?.left_child.?.right_child.?.point.value);
+}
